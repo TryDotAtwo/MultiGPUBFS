c808c0d882040bf5be429b962a27fa3411f3fe3a
Ivan Litvak <vanya.litvak@gmail.com>
2026-09-04 21:05:12 +0300

    kaggle: test buffered HF uploader

--- kaggle/hf-publish/kernel.py
+++ kaggle/hf-publish/kernel.py
@@ -13,7 +13,7 @@ from pathlib import Path
 
 from kaggle_secrets import UserSecretsClient
 
-SOURCE = "2b555312a6e622754314244032c8ba99bb0ba54b"
+SOURCE = "3d5d287ee0b7292bef170ed3405ca3566f0d6132"
 CUTLASS = "ffa119a1255d78998536107466cc7097ecefa393"
 REPO_ID = "TryDotAtwo/multigpubfs-bfs-results"
 RUST = "1.75.0"
